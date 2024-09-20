d2dde838fd4e83765bba86f06ad398a57f451b91
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2024-09-20 11:45:39 -0600

    Add files via upload

---
+++ humano1347.py
@@ -0,0 +1,58 @@
+print("                                          ")
+print("Actividad 9 clase humana")
+print("Isaac Jimenez Mat: 22308051281347")
+print("____________________________________________________________\n")
+# zona de clases 
+class Humano1347:
+    # zona de atribuos dentro de la clase
+    peso=0
+    nombre=0
+    fecha_de_nac=0
+    estatura=0
+    genero=0
+    # zona de funciones dentro de la clase
+    def correr1347(self,n):
+        print(f"{n} soy franchesco virgolini fiiuuuuum")
+    def comer1347(self,n):
+        print(f"{n} ñam ñam ñam ñan")
+    def patear1347(self,n):
+        print(f"{n} orale w3 una patada PUMM!!")
+    def volar1347(self,n):
+        print(f"{n} PUEDO VOLAAAAAAR!!!")
+
+# zona de creacion de objetos
+Isaac=Humano1347()
+agripina=Humano1347()
+
+#zona de usando objetos
+print("Resultados para Isaac")
+Isaac.peso=70
+Isaac.nombre="Isaac Jimenez"
+Isaac.estatura=1.75
+Isaac.genero="masculino"
+Isaac.fecha_de_nac="15/03/2007"
+print(f"Peso :{Isaac.peso}")
+print(f"Fecha de nacimiento:  {Isaac.fecha_de_nac}")
+print(f"Genero :{Isaac.genero}")
+print(f"Estatura :{Isaac.estatura}")
+print(f"Nombre :{Isaac.nombre}")
+Isaac.correr1347("yo")
+Isaac.comer1347("isaac: ")
+Isaac.patear1347("isaac: ")
+Isaac.volar1347("isaac: ")
+
+print("________________________________________________________________\n")
+
+print("Resultados para agripna")
+agripina.peso=80
+agripina.nombre="Agripina"
+agripina.estatura=1.62
+agripina.genero="femenino"
+agripina.fecha_de_nac="04/09/2009"
+print(f"Peso :{agripina.peso}")
+print(f"Fecha de nacimiento:  {agripina.fecha_de_nac}")
+print(f"Genero :{agripina.genero}")
+print(f"Estatura :{agripina.estatura}")
+agripina.correr1347("agripina: yo")
+agripina.patear1347("agripina: ")
+agripina.volar1347("agripina: ")
